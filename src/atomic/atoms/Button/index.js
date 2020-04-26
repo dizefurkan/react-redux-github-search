3b44646e9f10c344c3ff9c9e6021232263e08435
Said Furkan Dize <dizefurkan@gmail.com>
2020-04-26 23:38:20 +0300

    detail modal button and box shadow

--- src/atomic/atoms/Button/index.js
+++ src/atomic/atoms/Button/index.js
@@ -2,11 +2,12 @@ import React from 'react';
 
 import Text from '../Text';
 
-const Button = ({ text, onClick }) => {
+const Button = ({ text, onClick, className }) => {
+  const buttonClass = 'bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded ' + className;
   return (
     <button
       onClick={onClick}
-      className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+      className={buttonClass}
     >
       <Text text={text} />
     </button>
--- src/atomic/atoms/Modal/index.js
+++ src/atomic/atoms/Modal/index.js
@@ -4,9 +4,9 @@ import Button from '../Button';
 
 const Modal = ({ children, closeModal }) => {
   return (
-    <div className="fixed overflow-scroll w-64 h-full inset-0 bg-white pl-10">
+    <div className="fixed overflow-scroll w-64 h-full inset-0 bg-white pl-10 shadow-2xl">
       <div className="centered max-width-500 max-height-500 ">
-        <Button text="Close" onClick={closeModal} />
+        <Button text="Close" onClick={closeModal} className="my-4 w-full" />
         {children}
       </div>
     </div>
