f6361117814f1b12eb68096cdd5718a706b81b56
Said Furkan Dize <dizefurkan@gmail.com>
2020-04-26 23:44:28 +0300

    undefined class fix on button

--- src/atomic/atoms/Button/index.js
+++ src/atomic/atoms/Button/index.js
@@ -2,8 +2,8 @@ import React from 'react';
 
 import Text from '../Text';
 
-const Button = ({ text, onClick, className }) => {
-  const buttonClass = 'bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded ' + className;
+const Button = ({ text, onClick, className = '' }) => {
+  const buttonClass = `bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded ${className}`;
   return (
     <button
       onClick={onClick}
